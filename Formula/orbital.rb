136dc354706202a40f7393b337ccde1ce23e71ea
srmdn <mail@saidwp.com>
2026-05-15 21:29:01 +0700

    orbital v0.2.0 formula

---
+++ Formula/orbital.rb
@@ -0,0 +1,22 @@
+class Orbital < Formula
+  desc "macOS disk cleanup tool with developer awareness"
+  homepage "https://github.com/srmdn/orbital"
+  version "0.2.0"
+
+  on_arm do
+    url "https://github.com/srmdn/orbital/releases/download/v0.2.0/orbital_darwin_arm64"
+    sha256 "2fb8c92bf4fd1da6be0633d62aa7f65e219d6203faed9e9e1a9cd76256609a94"
+  end
+  on_intel do
+    url "https://github.com/srmdn/orbital/releases/download/v0.2.0/orbital_darwin_amd64"
+    sha256 "61dd9279703b2717d05a49cee6df508d67bfbdfae75743dc134687cabafc3360"
+  end
+
+  def install
+    bin.install Dir["orbital_darwin_*"] => "orbital"
+  end
+
+  test do
+    assert_match "orbital", shell_output("#{bin}/orbital version")
+  end
+end

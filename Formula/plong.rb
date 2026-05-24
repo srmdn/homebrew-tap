2eb6aad4449e0c7870216d9a416e900d82285c64
srmdn <mail@saidwp.com>
2026-05-24 16:36:44 +0700

    plong: bump to v0.2.4

--- Formula/plong.rb
+++ Formula/plong.rb
@@ -1,15 +1,15 @@
 class Plong < Formula
   desc "macOS disk cleanup tool with developer awareness"
   homepage "https://github.com/srmdn/plong"
-  version "0.2.3"
+  version "0.2.4"
 
   on_arm do
-    url "https://github.com/srmdn/plong/releases/download/v0.2.3/plong_darwin_arm64"
-    sha256 "5a92212300414b094b59cb82005f0f02460f8189d4250fda8be76b45aa2b38e2"
+    url "https://github.com/srmdn/plong/releases/download/v0.2.4/plong_darwin_arm64"
+    sha256 "71c308f17e86ecbaff2146d2cded6189ea3c9e96631f802cbcae69aaf621cfb6"
   end
   on_intel do
-    url "https://github.com/srmdn/plong/releases/download/v0.2.3/plong_darwin_amd64"
-    sha256 "50ae15bf3221567b999bac3a9a25f01ee824a7aef3ef0306b75d2a1a4d839d31"
+    url "https://github.com/srmdn/plong/releases/download/v0.2.4/plong_darwin_amd64"
+    sha256 "1260bccdc75f162f3101276509831d93742c63664304bd9f9b321ee710996ffb"
   end
 
   def install
